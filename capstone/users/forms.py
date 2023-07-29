5662a7e1d635987dae7b7613b7de100ec957e680
kaemonpillay <kaemonpillay@gmail.com>
2023-07-29 15:53:13 +0200

    added docstring to UserRegistrationForm class in users/forms

--- capstone/users/forms.py
+++ capstone/users/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserRegistrationForm(UserCreationForm):
+    """This creates a class for the user registration forms that will be used in registration of a user"""
     first_name = forms.CharField(max_length=101)
     last_name = forms.CharField(max_length=101)
     email = forms.EmailField()
