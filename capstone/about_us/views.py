7430bb6816a5f6a0f5f2f746c84dff59b9e06a1b
kaemonpillay <kaemonpillay@gmail.com>
2023-07-29 15:50:58 +0200

    added docstring to about_us function is about_us/views

--- capstone/about_us/views.py
+++ capstone/about_us/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about_us(request):
+    """Created the html request that will be triggered when this about_us page is called"""
     return render(request, "about_us.html")
