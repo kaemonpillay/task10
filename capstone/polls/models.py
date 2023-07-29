a6a8f8511a85e2134a3d3c61563616709e34ed7b
kaemonpillay <kaemonpillay@gmail.com>
2023-07-29 15:55:36 +0200

    added docstring to Question class in polls/models

--- capstone/polls/models.py
+++ capstone/polls/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
+    """Creates a call Question that will be used later in the polls page"""
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
